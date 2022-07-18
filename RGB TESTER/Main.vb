71fd8b000a7dcb15af0f94c9ef16f9a7c4c19c52
inaya_jp <mail@m.ta-yanai.com>
2022-07-18 12:22:06 +0900

    build:202207181222

--- RGB TESTER/Main.vb
+++ RGB TESTER/Main.vb
@@ -26,6 +26,66 @@
 
             If e.Column = 8 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
         End If
+
+        If e.Row = 1 Then
+            If e.Column = 0 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
+
+            If e.Column = 1 Then e.Graphics.FillRectangle(Brushes.White, e.CellBounds)
+
+            If e.Column = 2 Then e.Graphics.FillRectangle(Brushes.Yellow, e.CellBounds)
+
+            If e.Column = 3 Then e.Graphics.FillRectangle(Brushes.Cyan, e.CellBounds)
+
+            If e.Column = 4 Then e.Graphics.FillRectangle(Brushes.Green, e.CellBounds)
+
+            If e.Column = 5 Then e.Graphics.FillRectangle(Brushes.Magenta, e.CellBounds)
+
+            If e.Column = 6 Then e.Graphics.FillRectangle(Brushes.Red, e.CellBounds)
+
+            If e.Column = 7 Then e.Graphics.FillRectangle(Brushes.Blue, e.CellBounds)
+
+            If e.Column = 8 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
+        End If
+
+        If e.Row = 2 Then
+            If e.Column = 0 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
+
+            If e.Column = 1 Then e.Graphics.FillRectangle(Brushes.White, e.CellBounds)
+
+            If e.Column = 2 Then e.Graphics.FillRectangle(Brushes.Yellow, e.CellBounds)
+
+            If e.Column = 3 Then e.Graphics.FillRectangle(Brushes.Cyan, e.CellBounds)
+
+            If e.Column = 4 Then e.Graphics.FillRectangle(Brushes.Green, e.CellBounds)
+
+            If e.Column = 5 Then e.Graphics.FillRectangle(Brushes.Magenta, e.CellBounds)
+
+            If e.Column = 6 Then e.Graphics.FillRectangle(Brushes.Red, e.CellBounds)
+
+            If e.Column = 7 Then e.Graphics.FillRectangle(Brushes.Blue, e.CellBounds)
+
+            If e.Column = 8 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
+        End If
+
+        If e.Row = 3 Then
+            If e.Column = 0 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
+
+            If e.Column = 1 Then e.Graphics.FillRectangle(Brushes.White, e.CellBounds)
+
+            If e.Column = 2 Then e.Graphics.FillRectangle(Brushes.Yellow, e.CellBounds)
+
+            If e.Column = 3 Then e.Graphics.FillRectangle(Brushes.Cyan, e.CellBounds)
+
+            If e.Column = 4 Then e.Graphics.FillRectangle(Brushes.Green, e.CellBounds)
+
+            If e.Column = 5 Then e.Graphics.FillRectangle(Brushes.Magenta, e.CellBounds)
+
+            If e.Column = 6 Then e.Graphics.FillRectangle(Brushes.Red, e.CellBounds)
+
+            If e.Column = 7 Then e.Graphics.FillRectangle(Brushes.Blue, e.CellBounds)
+
+            If e.Column = 8 Then e.Graphics.FillRectangle(Brushes.Gray, e.CellBounds)
+        End If
     End Sub
 
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -45,8 +105,66 @@
 
         lbl8.BackColor = Color.Blue
 
-        lbl9.BackColor = Color.Blue
-
         lbl9.BackColor = Color.Gray
+
+
+        lbl10.BackColor = Color.Gray
+
+        lbl11.BackColor = Color.White
+
+        lbl12.BackColor = Color.Yellow
+
+        lbl13.BackColor = Color.Cyan
+
+        lbl14.BackColor = Color.Green
+
+        lbl15.BackColor = Color.Magenta
+
+        lbl16.BackColor = Color.Red
+
+        lbl17.BackColor = Color.Blue
+
+        lbl18.BackColor = Color.Gray
+
+
+
+        lbl19.BackColor = Color.Gray
+
+        lbl20.BackColor = Color.White
+
+        lbl21.BackColor = Color.Yellow
+
+        lbl22.BackColor = Color.Cyan
+
+        lbl23.BackColor = Color.Green
+
+        lbl24.BackColor = Color.Magenta
+
+        lbl25.BackColor = Color.Red
+
+        lbl26.BackColor = Color.Blue
+
+        lbl27.BackColor = Color.Gray
+
+
+
+        lbl28.BackColor = Color.Gray
+
+        lbl29.BackColor = Color.White
+
+        lbl30.BackColor = Color.Yellow
+
+        lbl31.BackColor = Color.Cyan
+
+        lbl32.BackColor = Color.Green
+
+        lbl33.BackColor = Color.Magenta
+
+        lbl34.BackColor = Color.Red
+
+        lbl35.BackColor = Color.Blue
+
+        lbl36.BackColor = Color.Gray
+
     End Sub
 End Class
